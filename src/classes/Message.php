19c2e04f4b70af28c093e78064c8eef3613d0559
Olga <olga@student.edu>
2017-07-26 13:53:17 +0200

    zmiany

---
+++ src/classes/Message.php
@@ -0,0 +1,110 @@
+<?php
+
+class Message {
+
+    private $id;
+    private $creationDate;
+    private $text;
+    private $messageRead;
+    private $senderId;
+    private $receiverId;
+
+    public function __construct() {
+        $this->id = -1;
+        $this->messageRead = 0;
+        $this->text = "";
+        $this->creationDate = "";
+        $this->senderId = "";
+        $this->receiverId = "";
+    }
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getCreationDate() {
+        return $this->creationDate;
+    }
+
+    function getText() {
+        return $this->text;
+    }
+
+    function getMessageRead() {
+        return $this->messageRead;
+    }
+
+    function getSenderId() {
+        return $this->senderId;
+    }
+
+    function getReceiverId() {
+        return $this->receiverId;
+    }
+
+    function setCreationDate($creationDate) {
+        $this->creationDate = $creationDate;
+    }
+
+    function setText($text) {
+        $this->text = $text;
+    }
+
+    function setMessageRead($messageRead) {
+        $this->messageRead = $messageRead;
+    }
+
+    function setSenderId($senderId) {
+        $this->senderId = $senderId;
+    }
+
+    function setReceiverId($receiverId) {
+        $this->receiverId = $receiverId;
+    }
+
+    public function saveToDB($connection) {
+        if ($this->id == -1) {
+            $sql = "INSERT INTO Message (id, text, creationDate, messageRead, senderId, receiverId) 
+                    VALUES ($this->id, $this->text, '$this->creationDate, $this->messageRead, $this->senderId, $this->receiverId')";
+            $result = $connection->query($sql);
+            if ($result == true) {
+                $this->id = $connection->insert_id;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static public function loadMessageBySenderId($senderId, mysqli $connection) {
+        $sql = 'SELECT * FROM Messages WHERE senderId = ' . $senderId;
+        $result = $connection->query($sql);
+
+        if ($result == true && $result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $loadedMessage = new Message();
+            $loadedMessage->assignValues($row['id'], $row['messageRead'], $row['text'], $row['creationDate'], $row['senderId'],$row['receiverId'] );
+
+            return $loadedMessage;
+        } else {
+            return null;
+        }
+    }
+
+    static public function loadAllMessagesBySenderId($senderId, mysqli $connection) {
+        $sql = "SELECT * FROM Comments WHERE PostId = '$PostId'";
+        $result = $connection->query($sql);
+        $comments = [];
+        if ($result == true && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $loadedComment = new Comment();
+                $loadedComment->id = $row['id'];
+                $loadedComment->userId = $row['userId'];
+                $loadedComment->text = $row['text'];
+                $loadedComment->creationDate = $row['creationDate'];
+                $loadedComment->userId = $row['PostId'];
+                $comments[] = $loadedComment;
+            }
+        }
+        return $comments;
+    }
+    
--- src/database/create_tables.php
+++ src/database/create_tables.php
@@ -59,10 +59,11 @@ $sql = "CREATE TABLE 'Message'(
     id INT NOT NULL AUTO_INCREMENT,
     text varchar(140) NOT NULL,
     creationDate DATE NOT NULL,
+    MessageRead INT NOT NULL,
     SenderId INT NOT NULL,
     ReceiverId INT NOT NULL,
     PRIMARY KEY id,
-    FOREIGN KEY (Senderid) REFERENCES Users(id),
+    FOREIsGN KEY (Senderid) REFERENCES Users(id),
     FOREIGN KEY (Receiverid) REFERENCES Users(id),
     )";
       
