43a47662b5ea10e86cce2ea46d5c762a1d33afdd
Olga <olga@student.edu>
2017-07-27 18:51:12 +0200

    zmiany

--- src/Websites/LogIn.php
+++ src/Websites/LogIn.php
@@ -2,48 +2,46 @@
 session_start();
 require_once '../src/connection.php';
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_POST['password'])){
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_POST['password'])) {
     $username = trim($_POST['email']);
     $password = trim($_POST['password']);
 }
 if (!empty($_POST['email']) && !empty($_POST['password'])) {
-        
-        $sql = "SELECT * FROM Users WHERE email = '$email'"; 
-        $query = $conn->query($sql);
-        
-        if ($query->num_rows > 0) {
-            $row = $query->fetch_assoc();                      
-            $hashed_password = $row['hashed_password'];
-            $checkPassword = password_verify($password, $hashed_password);
-            if ($checkPassword) {
-                $_SESSION['id'] = $row['id'];
-                //header("Location: ../tbd");
-            }
-        }
-}
-else {
-    echo "wrong username or password, please try againg";
-}
 
+    $sql = "SELECT * FROM Users WHERE email = '$email'";
+    $query = $conn->query($sql);
+
+    if ($query->num_rows > 0) {
+        $row = $query->fetch_assoc();
+        $hashed_password = $row['hashed_password'];
+        $checkPassword = password_verify($password, $hashed_password);
+        if ($checkPassword) {
+            $_SESSION['id'] = $row['id'];
+            //header("Location: ../tbd");
+        }
+    }
+} else {
+    echo "wrong username or password, please try again";
+}
 ?>
 
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-<form>
+    <form>
 
-<label for = "email">Username:</label>
+        <label for = "email">Username:</label>
 
-<input type = "text" name = "email" id = "Username"
+        <input type = "text" name = "email" id = "Username"
 
-placeholder = "Please provide your e-mail">
+               placeholder = "Please provide your e-mail">
 
-<label>Password:
+        <label>Password:
 
-<input type = "text" name = "password"
+            <input type = "text" name = "password"
 
-placeholder = "Please provide your password"></label>
+                   placeholder = "Please provide your password"></label>
 
-<input type = "submit" value = "Wyślij">
+        <input type = "submit" value = "Wyślij">
 
-</form>
+    </form>
 
