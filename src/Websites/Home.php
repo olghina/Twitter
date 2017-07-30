9bb21001a9418c123cc4be66f2c0851194d504f9
Olga <olga@student.edu>
2017-07-30 15:51:15 +0200

    zmiany

--- src/Websites/Home.php
+++ src/Websites/Home.php
@@ -1,26 +1,24 @@
 <?php
-
 session_start();
 require_once '../src./connection.php';
 
-if(!isset($_SESSION['userId'])){
+if (!isset($_SESSION['userId'])) {
     header('Location: Websites/LogIn.php');
 }
 
 $userSession = $_SESSION['userId'];
 $Userlogged = User::loadUserById($connect, $userSession);
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['newTweet']) && strlen(trim($_POST['newTweet'])) > 0){
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['newTweet']) && strlen(trim($_POST['newTweet'])) > 0) {
     $tweet = new Tweet();
     $tweet->setText($_POST['newTweet']);
     $tweet->setUserId($userSession);
     $tweet->setCreationDate(date('Y-m-d H:i:s'));
-    
-    if($tweet->saveToDB($connect)){
+
+    if ($tweet->saveToDB($connect)) {
         echo 'New tweet from user: ' . $Userlogged->getUsername() . '<br>' . 'Tweet content: '
-            . $_POST['newTweet'] . '<br>';
-    }
-    else{
+        . $_POST['newTweet'] . '<br>';
+    } else {
         echo 'please try again';
     }
 }
@@ -36,23 +34,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['newTweet']) && strlen(t
                 <label for ="newTweet"> your tweet </label>
                 <input type ="text" name ="newTweet" id="newTweet"
                        placeholder ="enter your Tweet here, 140 characters only">
-                       <input type = "submit" value = "add">
+                <input type = "submit" value = "add">
 
             </form>
         </div>
         <div>
             <p> list of all tweets </p>
             <?php
-            $tweets = Tweet::loadAllTweetsByUserId($userId, $connection);
-            foreach($tweets as $tweet){            
-            $authorTweetId = $tweet->getUserId();
-            $authorTweet = User::loadUserbyId($connect, $authorTweetId);    
-            
-            echo  . $authorTweet->getUsername() . 'added tweet at time:'. 
-                 $tweet->getCreationDate() . '<br> Content: </b>' . 
-                 $tweet->getText();
-           ?>
-      
+            $tweets = Tweet::loadAllTweets($connection);
+            foreach ($tweets as $tweet) {
+                $authorTweetId = $tweet->getUserId();
+                $authorTweet = User::loadUserById($id, $conn);
+                echo $authorTweet->getUsername() . '<br> added tweet at time:' .
+                $tweet->getCreationDate() . '<br> Content:' .
+                $tweet->getText();
+            }
+
+            //viewing comments to this tweet
+            $comments = Comment::loadAllCommentsByPostId($PostId, $connection);
+           $authorComment = User::loadUserById($id, $conn);
+                   
+                   
+            echo ' Author of the comment:' . $authorComment->getUsername() . '<br>';
+            echo ' Posted: ' . $comments->getCreationDate() . "<br>";
+            echo ' Content:' . $comments->getText() . "<br><br>";
+            ?>
+
         </div>
 
     </body>
